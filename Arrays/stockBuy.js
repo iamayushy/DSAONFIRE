7011a4f82a8354d50eb0e8b88c5dd2403f8df3df
iamayushy <ayushhuns@gmai.com>
2022-04-17 13:10:27 +0530

    stockbuy

--- Arrays/stockBuy.js
+++ Arrays/stockBuy.js
@@ -1,1 +1,12 @@
-console.log('Buy Some Stocks')
+function zero(arr){
+    let max = arr[0];
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < arr.length; i++){
+        min = Math.min(min, arr[i]);
+        max = Math.max(max, arr[i] - min);
+    }
+    return max;
+}
+
+const arr = [3, 2, 4, 1, 4, 6, 7];
+console.log(zero(arr));
