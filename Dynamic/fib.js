dfcce74ed0a2081d0faf45b779777e0d99953f50
iamayushy <ayushhuns@gmai.com>
2022-04-17 20:07:38 +0530

    added dp

--- Dynamic/fib.js
+++ Dynamic/fib.js
@@ -1,7 +1,11 @@
-function fib(n){
+//memoization
+
+function fib(n, memo = {}){
+    if(n in memo){
+        return memo[n]
+    }
     if(n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2)
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
 }
-console.log(fib(5))
-console.log(fib(6))
-console.log(fib(7))
+console.log(fib(400))
